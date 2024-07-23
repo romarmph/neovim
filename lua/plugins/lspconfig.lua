eb46af28fb10c36c41213d1de6a588873f664d68
Romar Macaraeg <romarmph@gmail.com>
2024-07-23 22:31:29 +0800

    Update plugins
    Add trouble in hardtime excemptions
    Switch to eslint-lsp for linting

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,76 +1,70 @@
 return {
-  'neovim/nvim-lspconfig',
-  dependencies = {
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
-    'WhoIsSethDaniel/mason-tool-installer.nvim',
-    {
-      "folke/lazydev.nvim",
-      ft = "lua", -- only load on lua files
-      opts = {
-        library = {
-          -- See the configuration section for more details
-          -- Load luvit types when the `vim.uv` word is found
-          "lazy.nvim",
-          { path = "luvit-meta/library", words = { "vim%.uv" } },
-        },
-      },
-    },
-  },
-  opts = {
-    lsp_doc_border = true,
-  },
-  config = function()
-    vim.api.nvim_create_autocmd('LspAttach', {
-      vim.diagnostic.config {
-        update_in_insert = true,
-      },
-      group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
-      callback = function(event)
-        local telescope = require("telescope.builtin");
-        require('plugins.lsp.keymaps').create_keymaps(event, telescope)
-        local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if client and client.server_capabilities.documentHighlightProvider then
-          vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-            buffer = event.buf,
-            callback = vim.lsp.buf.document_highlight,
-          })
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		{
+			"folke/lazydev.nvim",
+			ft = "lua", -- only load on lua files
+			opts = {
+				library = {
+					-- See the configuration section for more details
+					-- Load luvit types when the `vim.uv` word is found
+					"lazy.nvim",
+					{ path = "luvit-meta/library", words = { "vim%.uv" } },
+				},
+			},
+		},
+	},
+	opts = {
+		lsp_doc_border = true,
+	},
+	config = function()
+		vim.api.nvim_create_autocmd("LspAttach", {
+			vim.diagnostic.config({
+				update_in_insert = true,
+			}),
+			group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+			callback = function(event)
+				local telescope = require("telescope.builtin")
+				require("plugins.lsp.keymaps").create_keymaps(event, telescope)
+				local client = vim.lsp.get_client_by_id(event.data.client_id)
+				if client and client.server_capabilities.documentHighlightProvider then
+					vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+						buffer = event.buf,
+						callback = vim.lsp.buf.document_highlight,
+					})
 
-          vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-            buffer = event.buf,
-            callback = vim.lsp.buf.clear_references,
-          })
-        end
-      end
-    })
+					vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+						buffer = event.buf,
+						callback = vim.lsp.buf.clear_references,
+					})
+				end
+			end,
+		})
 
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = vim.tbl_deep_extend(
-      'force',
-      capabilities,
-      require('cmp_nvim_lsp').default_capabilities()
-    )
-
-
-    require("mason").setup()
-    local servers = require('plugins.lsp.servers').servers
-    local ensure_installed = vim.tbl_keys(servers or {})
-    vim.list_extend(ensure_installed, {
-      'stylua',
-      'tsserver',
-      'eslint-lsp',
-      'prettier'
-    })
-    require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-    require('mason-lspconfig').setup {
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
-          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-          require('lspconfig')[server_name].setup(server)
-        end,
-      },
-    }
-  end
+		require("mason").setup()
+		local servers = require("plugins.lsp.servers").servers
+		local ensure_installed = vim.tbl_keys(servers or {})
+		vim.list_extend(ensure_installed, {
+			"stylua",
+			"tsserver",
+			"eslint-lsp",
+			"prettier",
+		})
+		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+		require("mason-lspconfig").setup({
+			handlers = {
+				function(server_name)
+					local server = servers[server_name] or {}
+					server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+					require("lspconfig")[server_name].setup(server)
+				end,
+			},
+		})
+	end,
 }
