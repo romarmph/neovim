2e34b18f08256afa65179c2d57609f850c7fa3fe
Romar Macaraeg <romarmph@gmail.com>
2024-07-29 02:26:51 +0800

    Change theme back to tokyonight

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -31,6 +31,7 @@ return {
           ['<CR>'] = cmp.mapping.confirm({ select = true }),
         }),
         sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'buffer' },
           { name = 'path' },
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,8 +1,25 @@
 return {
   {
     "tiagovla/tokyodark.nvim",
-    config = function(_, opts)
+    cond = false,
+    config = function(_)
       require("plugins.theme.tokyodark")
     end,
-},
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup({
+        transparent = true,
+        terminal_colors = true,
+        styles = {
+          sidebars = 'transparent',
+          floats = 'transparent',
+        },
+      })
+      vim.cmd [[colorscheme tokyonight]]
+    end
+  }
 }
